b026ea5837dbbfc4cdc6f4c74b36dfa679167939
yusufhumayun60@gmail.com <yusufhumayun60@gmail.com>
2022-04-17 11:53:57 +0530

    Demo Java File

---
+++ DemoTest.java
@@ -0,0 +1,21 @@
+class DemoTest
+{
+    int x; //default
+    private int y;
+    public int z;
+    protected int a;
+
+    public void show()
+    {
+
+    }
+
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World");
+        System.out.print("Hello World\n");
+        System.out.printf("%s","Hello World");
+         System.out.printf("Number :%d",30);
+
+    }
+}
